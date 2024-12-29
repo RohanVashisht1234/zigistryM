51c1f7b1fdccece92ea1cf6d809e4b80c654ff01
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-12-30 01:07:01 +0530

    ok

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{marko,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
